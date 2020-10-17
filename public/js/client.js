5f4e1971a030e2406c283fdf9473b8dfb4ced5c8
Chidalu Agbakwa <cagbakwa@gmail.com>
2020-10-17 19:07:58 -0400

    Added Bootstrap for styling

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,11 @@
 const socket = io();
 
 socket.on('connect', () => {
-    document.getElementsByClassName('chatArea').innerText = "A Connection Happened!";
-})
+    // const queryString = window.location.search;
+    // const urlParams = new URLSearchParams(queryString);
+    // console.log(`<div>${urlParams.get('userName')}`);
+    // const userEnteredHTML = `<div>${urlParams.get('userName')} has joined the room</div>`;
+    // document.getElementById('content').appendChild(userEnteredHTML);
+
+
+});
